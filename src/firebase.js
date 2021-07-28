438c0bbd242ffa6c460e2143dce7e18d8464a6f0
Your Name <yourname@example.com>
2021-07-28 15:50:23 +0530

    header ready

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDxJUq-ikApMsV0XTZS-LPEIvAtQZlFXIo",
+    authDomain: "clone-3fb87.firebaseapp.com",
+    projectId: "clone-3fb87",
+    storageBucket: "clone-3fb87.appspot.com",
+    messagingSenderId: "808737661622",
+    appId: "1:808737661622:web:515218d9272c839a017c17",
+    measurementId: "G-QGEGK3YF5S"
+  };
